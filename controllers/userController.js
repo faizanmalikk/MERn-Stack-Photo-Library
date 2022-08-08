2b77ae4c1f0a060834bbfb4ea61446c06a31a912
faizanmalykk <faizanjamil656@gmail.com>
2022-08-08 15:11:44 +0500

    updated

--- controllers/userController.js
+++ controllers/userController.js
@@ -48,14 +48,8 @@ exports.logoutUser = asyncErrors(async (req, res, next) => {
 
     res.cookie('token', null, {
         expires: new Date(Date.now()),
-        httpOnly: true,
-        sameSite : 'none',
-        secure : true
+        httpOnly: true
     })
-
-
-
-            
 
     res.status(200).json({
         success: true,
--- utils/JwtToken.js
+++ utils/JwtToken.js
@@ -9,9 +9,7 @@ const sendToken = (user, statusCode, res) => {
         expires: new Date(
             Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
         ),
-        httpOnly: true,
-        sameSite : 'none',
-        secure : true
+        httpOnly: true
     }
 
 
